ff7f5f570c6edcfd80907dde4cf1a56984043077
mccharliesins <vkyd1909@gmail.com>
2025-06-23 09:12:20 -0400

    feat(infra): add ecs cluster and ecr repository to stack

--- infra/lib/ecs-stack.ts
+++ infra/lib/ecs-stack.ts
@@ -1,8 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
 import { Construct } from 'constructs';
 
 export class EcsStack extends cdk.Stack {
+    public readonly ecrRepo: ecr.Repository;
+
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
@@ -10,6 +14,18 @@ export class EcsStack extends cdk.Stack {
         const vpc = new ec2.Vpc(this, 'AppVpc', {
             maxAzs: 2,
             natGateways: 1,
+        });
+
+        // ecs cluster
+        const cluster = new ecs.Cluster(this, 'AppCluster', {
+            vpc: vpc,
+            clusterName: 'ha-cluster',
+        });
+
+        // ecr repository
+        this.ecrRepo = new ecr.Repository(this, 'AppRepo', {
+            repositoryName: 'ha-app-repo',
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
         });
     }
 }
